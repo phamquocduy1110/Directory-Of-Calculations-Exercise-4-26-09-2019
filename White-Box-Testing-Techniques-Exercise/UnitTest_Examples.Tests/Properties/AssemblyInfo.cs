688c12a818cc41ea09e86939f784bcd4da996697
Phamquocduy1110@gmail.com <Sakuraandusagi*123>
2020-04-24 22:27:46 +0700

    Create UnitTest_Example in Class Library (.NET Framework) and create UnitTest_Examples.Tests in Unit Test Project (.NET Framework)

---
+++ White-Box-Testing-Techniques-Exercise/UnitTest_Examples.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Examples.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Examples.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce08f3da-ea3a-4f23-85cf-bdfa5b725c63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
