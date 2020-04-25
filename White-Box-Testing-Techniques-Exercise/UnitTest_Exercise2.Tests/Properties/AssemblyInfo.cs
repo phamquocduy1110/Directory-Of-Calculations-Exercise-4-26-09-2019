62bb72dd2d189365f4f1256967bcb9c137aaff79
Phamquocduy1110@gmail.com <Sakuraandusagi*123>
2020-04-25 07:56:58 +0700

    Create UnitTest_Exercise 2 in Class Library (.NET Framework) and create UnitTest_Exercis2.Tests in Unit Test Project (.NET Framework)

---
+++ White-Box-Testing-Techniques-Exercise/UnitTest_Exercise2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Exercise2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Exercise2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ec0c95f-db79-4688-a8bd-7de7bd9e8574")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
