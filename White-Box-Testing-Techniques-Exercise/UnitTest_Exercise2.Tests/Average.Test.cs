581260e41164d8b2494091f780b698e408c8bb64
Phamquocduy1110@gmail.com <Sakuraandusagi*123>
2020-05-04 07:16:06 +0700

    Complete the remaining test cases

--- White-Box-Testing-Techniques-Exercise/UnitTest_Exercise2.Tests/Average.Test.cs
+++ White-Box-Testing-Techniques-Exercise/UnitTest_Exercise2.Tests/Average.Test.cs
@@ -207,7 +207,93 @@ namespace UnitTest_Exercise2.Tests
             }
 
             minnum = 1;
-            maxnum = 99;
+            maxnum = 50;
+
+            while ((array_value[i] != -999) && (inputNumber < 100))
+            {
+                inputNumber++;
+                if ((array_value[i] >= minnum) && (array_value[i] <= maxnum))
+                {
+                    validNumber++;
+                    sum += array_value[i];
+                }
+                else break;
+                i++;
+            }
+
+            if (validNumber > 0)
+                avg = sum / validNumber;
+            else
+                avg = -999;
+
+            float expectedResult = avg;
+
+            //Action
+            float actualResult = average.average(array_value, minnum, maxnum);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        ///  TC7: Test array wiht 100 elements { 1,...,100 }, minnum = 1, maxnum = 100. The actualResult: <50>.
+        /// </summary>
+        [TestMethod]
+        public void TestCase7()
+        {
+            //Arrange
+            int[] array_value = new int[100];
+
+            for (var i = 1; i < array_value.Length; i++)
+            {
+                array_value[i] = i;
+            }
+
+            minnum = 1;
+            maxnum = 100;
+
+            while ((array_value[i] != -999) && (inputNumber < 100))
+            {
+                inputNumber++;
+                if ((array_value[i] >= minnum) && (array_value[i] <= maxnum))
+                {
+                    validNumber++;
+                    sum += array_value[i];
+                }
+                else break;
+                i++;
+            }
+
+            if (validNumber > 0)
+                avg = sum / validNumber;
+            else
+                avg = -999;
+
+            float expectedResult = avg;
+
+            //Action
+            float actualResult = average.average(array_value, minnum, maxnum);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        ///  TC8: Test array wiht 100 elements { 1,...,101 }, minnum = 1, maxnum = 101. The actualResult: <50>.
+        /// </summary>
+        [TestMethod]
+        public void TestCase8()
+        {
+            //Arrange
+            int[] array_value = new int[100];
+
+            for (var i = 1; i < array_value.Length; i++)
+            {
+                array_value[i] = i;
+            }
+
+            minnum = 1;
+            maxnum = 101;
 
             while ((array_value[i] != -999) && (inputNumber < 100))
             {
@@ -271,6 +357,172 @@ namespace UnitTest_Exercise2.Tests
 
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        // <summary>
+        ///  TC10: Test array null, minnum = 30, maxnum = 70. The actualResult: <Error>.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestCase10()
+        {
+            //Arrange
+            int[] array_value = null;
+
+            minnum = 30;
+            maxnum = 70;
+
+            while ((array_value[i] != -999) && (inputNumber < 100))
+            {
+                inputNumber++;
+                if ((array_value[i] >= minnum) && (array_value[i] <= maxnum))
+                {
+                    validNumber++;
+                    sum += array_value[i];
+                }
+                else break;
+                i++;
+            }
+
+            if (validNumber > 0)
+                avg = sum / validNumber;
+            else
+                avg = -999;
+
+            float expectedResult = avg;
+
+            //Action
+            float actualResult = average.average(array_value, minnum, maxnum);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        ///  TC11: Test array with 6 elements { 10, 30, 40, 70, 99, 50 }, minnum = null, maxnum = 99. The actualResult: <Error>.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestCase11()
+        {
+            //Arrange
+            int[] array = new int[] { 10, 30, 40, 70, 99, 50 };
+
+            maxnum = 99;
+
+            while ((array[i] != -999) && (inputNumber < 100))
+            {
+                inputNumber++;
+                if ((array[i] >= minnum) && (array[i] <= maxnum))
+                {
+                    validNumber++;
+                    sum += array[i];
+                }
+                else break;
+                i++;
+            }
+
+            if (validNumber > 0)
+                avg = sum / validNumber;
+            else
+                avg = -999;
+
+            float expectedResult = avg;
+
+            //Action
+            float actualResult = average.average(array, minnum, maxnum);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        ///  TC12: Test array with 3 elements { 20, 50, 68 }, minnum = 30, maxnum = null. The actualResult: <Error>.
+        /// </summary>
+        [TestMethod]
+        ///[ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestCase12()
+        {
+            //Arrange
+            int[] array = new int[] { 20, 50, 68 };
+
+            minnum = 30;
+
+            while ((array[i] != -999) && (inputNumber < 100))
+            {
+                inputNumber++;
+                if ((array[i] >= minnum) && (array[i] <= maxnum))
+                {
+                    validNumber++;
+                    sum += array[i];
+                }
+                else break;
+                i++;
+            }
+
+            if (validNumber > 0)
+                avg = sum / validNumber;
+            else
+                avg = -999;
+
+            float expectedResult = "Error message";
+
+            //Action
+            float actualResult = average.average(array, minnum, maxnum);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        ///  TC13: Test array with 100 elements { 10, 45, 50, 68, 90, 99 }, minnum = 40.1, maxnum = 70. The actualResult: <error>.
+        /// </summary>
+        [TestMethod]
+        public void TesCase13()
+        {
+            //Arrange
+            int[] array_value = new int[100];
+
+            for (var i = 1; i < array_value.Length; i++)
+            {
+                array_value[i] = i;
+            }
+
+            double minnum = 40.1;
+            double maxnum = 70;
+
+            if (array_value.Length > 0 && minnum != 0 && maxnum != 0)
+            {
+                if (minnum * 10 == (int)minnum * 10)
+                {
+                    while ((array_value[i] != -999) && (inputNumber < 100))
+                    {
+                        inputNumber++;
+                        if ((array_value[i] >= minnum) && (array_value[i] <= maxnum))
+                        {
+                            validNumber++;
+                            sum += array_value[i];
+                        }
+                        else break;
+                        i++;
+                    }
+
+                    if (validNumber > 0)
+                        avg = sum / validNumber;
+                    else
+                        avg = -999;
+
+                    float expectedResult = avg;
+
+                    //Action
+                    float actualResult = average.average(array_value, minnum, maxnum);
+
+                    //Assert
+                    Assert.AreEqual(expectedResult, actualResult);
+                }
+            }
+            string expectedResult1 = "Error message";
+            Assert.AreEqual(expectedResult1, "Error message");
         }
     }
 }
